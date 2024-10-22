f95172fd132863f95b949f34d51ed555ed431325
TheArabicMonster <neetamk@icloud.com>
2024-10-22 15:11:25 +0200

    feat(test unitaire): fichier de test unitaire ajouté

---
+++ Code/RiftMetricsTests/MainWindowTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OxyPlot;
+using OxyPlot.Wpf;
+using RiftMetrics;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Controls;
+
+namespace RiftMetrics.Tests
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+
+    }
+}
